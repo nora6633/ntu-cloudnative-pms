f44eb37c297814d55cd8b48a1290863af1066463
peterncs125 <r14922186@csie.ntu.edu.tw>
2026-04-30 10:17:51 +0000

    Add condition for mapping employee and supervisor details in EvaluationMapper

--- backend/src/main/java/edu/ntu/pms/evaluation/mapper/EvaluationMapper.java
+++ backend/src/main/java/edu/ntu/pms/evaluation/mapper/EvaluationMapper.java
@@ -1,7 +1,9 @@
 package edu.ntu.pms.evaluation.mapper;
 
+import org.mapstruct.Condition;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 import edu.ntu.pms.evaluation.dto.EvaluationDTO;
 import edu.ntu.pms.evaluation.dto.EvaluationItemDTO;
@@ -11,6 +13,7 @@ import edu.ntu.pms.evaluation.entity.Evaluation;
 import edu.ntu.pms.evaluation.entity.EvaluationItem;
 import edu.ntu.pms.evaluation.entity.Goal;
 import edu.ntu.pms.evaluation.entity.Progress;
+import edu.ntu.pms.evaluation.enums.EvaluationStatus;
 
 @Mapper(componentModel = "spring")
 public interface EvaluationMapper {
@@ -18,7 +21,19 @@ public interface EvaluationMapper {
     // Entity to DTO mappings
 
     // Top Level
+    @Mapping(source = "employee.username", target = "employeeName", conditionQualifiedByName = "isNotClosed")
+    @Mapping(source = "employee.job.title", target = "employeeJobTitle", conditionQualifiedByName = "isNotClosed")
+    @Mapping(source = "employee.department.name", target = "employeeDepartmentName", conditionQualifiedByName = "isNotClosed")
+    @Mapping(source = "supervisor.username", target = "supervisorName", conditionQualifiedByName = "isNotClosed")
+    @Mapping(source = "supervisor.job.title", target = "supervisorJobTitle", conditionQualifiedByName = "isNotClosed")
+    @Mapping(source = "supervisor.department.name", target = "supervisorDepartmentName", conditionQualifiedByName = "isNotClosed")
     EvaluationDTO toDto(Evaluation entity);
+
+    @Condition
+    @Named("isNotClosed")
+    default boolean isNotClosed(Evaluation entity) {
+        return entity.getStatus() != EvaluationStatus.CLOSED;
+    }
 
     // Mid Level: MapStruct uses this automatically for the List<GoalDTO>
     GoalDTO toGoalDto(Goal entity);
